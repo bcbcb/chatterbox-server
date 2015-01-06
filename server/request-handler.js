980a73ca7f92a62a263fcff509718a6170a4ea75
bcbcb <bcbcb@users.noreply.github.com>
2015-01-05 16:50:41 -0800

    Completed Features that made ServerSpec.js pass

--- server/request-handler.js
+++ server/request-handler.js
@@ -27,19 +27,34 @@ exports.requestHandler = function(request, response) {
   // Adding more logging to your server can be an easy way to get passive
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
-  // The outgoing status.
-  var statusCode = 200;
+  //console.log("Serving request type " + request.method + " for url " + request.url);
 
   // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
+  var data = {results: []};
+  // The outgoing status.
+  var statusCode;
+
+  if (request.method === 'GET') {
+    if (db[request.url] === undefined) {
+      statusCode = 404;
+    } else {
+      statusCode = 200;
+      data.results = db[request.url];
+    }
+  } else if (request.method === 'POST') {
+    request.on('data', function(data){
+      db[request.url].push(JSON.parse(data));
+    });
+    statusCode = 201;
+  }
+
 
   // Tell the client we are sending them plain text.
   //
   // You will need to change this if you are sending something
   // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = "text/plain";
+  headers['Content-Type'] = "application/json";
 
   // .writeHead() writes to the request line and headers of the response,
   // which includes the status and all headers.
@@ -52,7 +67,11 @@ exports.requestHandler = function(request, response) {
   //
   // Calling .end "flushes" the response's internal buffer, forcing
   // node to actually send all the data over to the client.
-  response.end("Hello, World!");
+  response.end(JSON.stringify(data));
+};
+
+var db = {
+  "/classes/room1": []
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
